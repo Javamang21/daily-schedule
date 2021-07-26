72b41101e212c5fb65a42ff8aa04af92c16b3a7b
Javamang21 <juliustovar96@gmail.com>
2021-07-25 23:12:18 -0400

    added all the if statements now i just need to keep each tasked saved to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var n = d.getHours();
 console.log(n);
 */
 
-// After getting the current hour we need to create a statement that allows us to compare timeNow variable with the present, past and future classes.
+// After getting the current hour we need to create a statement that allows us to compare timeNow variable with the present, past and future classes to give each log its color.
 
 var timeNow = new Date().getHours();
 console.log(timeNow);
@@ -20,11 +20,85 @@ const colorSlot = () => {
     $("#nineam").addClass("present");
 }
     else if(timeNow < 9) {
-    $("#nineam").addClass("future")
+    $("#nineam").addClass("future");
 }
     else if(timeNow > 9) {
     $("#nineam").addClass("past");
 }
+if(timeNow === 10) {
+    $("#tenam").addClass("present");
 }
+    else if(timeNow < 10) {
+    $("#tenam").addClass("future");
+}
+    else if(timeNow > 10) {
+    $("#tenam").addClass("past");
+}
+if(timeNow === 11) {
+    $("#elevenam").addClass("present");
+}
+    else if(timeNow < 11) {
+    $("#elevenam").addClass("future");
+}
+    else if(timeNow > 11) {
+    $("#elevenam").addClass("past");
+}
+if(timeNow === 12) {
+    $("#twelvepm").addClass("present");
+}
+    else if(timeNow < 12) {
+    $("#twelvepm").addClass("future")
+}
+    else if(timeNow > 12) {
+    $("#twelvepm").addClass("past");
+}
+if(timeNow === 13) {
+    $("#onepm").addClass("present");
+}
+    else if(timeNow < 13) {
+    $("#onepm").addClass("future");
+}
+    else if(timeNow > 13) {
+    $("#onepm").addClass("past");
+}
+if(timeNow === 14) {
+    $("#twopm").addClass("present");
+}
+    else if(timeNow < 14) {
+    $("#twopm").addClass("future");
+}
+    else if(timeNow > 14) {
+    $("#twopm").addClass("past");
+}
+if(timeNow === 15) {
+    $("#threepm").addClass("present");
+}
+    else if(timeNow < 15) {
+    $("#threepm").addClass("future");
+}
+    else if(timeNow > 15) {
+    $("#threepm").addClass("past");
+}
+if(timeNow === 16) {
+    $("#fourpm").addClass("present");
+}
+    else if(timeNow < 16) {
+    $("#fourpm").addClass("future");
+}
+    else if(timeNow > 16) {
+    $("#fourpm").addClass("past");
+}
+if(timeNow === 17) {
+    $("#fivepm").addClass("present");
+}
+    else if(timeNow < 17) {
+    $("#fivepm").addClass("future");
+}
+    else if(timeNow > 17) {
+    $("#fivepm").addClass("past");
+}
+};
 
-console.log(colorSlot);
+
+window.onload = colorSlot();
+    
